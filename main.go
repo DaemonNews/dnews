7152ee9c270413df1dc36725f7644359bc668fb2
Aaron Bieber <aaron@bolddaemon.com>
2016-08-19 17:24:16 -0600

    mostly functional sessions

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 
-	// Gimme that sweet sweet pg
-
+	"github.com/gorilla/sessions"
 	"github.com/qbit/dnews/src"
 )
+
+var store = sessions.NewCookieStore([]byte("something-very-secret"))
 
 func getArticles(w http.ResponseWriter, r *http.Request) {
 
@@ -26,13 +28,46 @@ var funcMap = template.FuncMap{
 	},
 }
 
-var idxT, err = template.New("dnews").Funcs(funcMap).ParseGlob("templates/*.html")
+var templ, err = template.New("dnews").Funcs(funcMap).ParseGlob("templates/*.html")
 
 func main() {
 	fs := http.FileServer(http.Dir("public"))
 
 	http.Handle("/public/", http.StripPrefix("/public/", fs))
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		session, err := store.Get(r, "session-name")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		user := r.FormValue("user")
+		passwd := r.FormValue("passwd")
+
+		if user == "" && passwd == "" {
+			err = templ.ExecuteTemplate(w, "login.html", nil)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		} else {
+			// do auth
+			log.Printf("Authed %s", user)
+			session.Values["user"] = dnews.User{}
+			fmt.Printf("%v", session.Values["user"])
+			//session.Values["user"].Authed = true
+
+			session.Save(r, w)
+			http.Redirect(w, r, "/", http.StatusFound)
+		}
+	})
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		session, err := store.Get(r, "session-name")
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
 		a, err := dnews.GetNArticles(10)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -42,7 +77,20 @@ func main() {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		err = idxT.ExecuteTemplate(w, "index.html", a)
+
+		var u = dnews.User{}
+		u.Authed = true
+		u.FName = "sucka"
+		fmt.Println(u)
+
+		data := struct {
+			Articles *dnews.Articles
+		}{
+			&a,
+		}
+		session.Save(r, w)
+		err = templ.ExecuteTemplate(w, "header.html", u)
+		err = templ.ExecuteTemplate(w, "index.html", data)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
